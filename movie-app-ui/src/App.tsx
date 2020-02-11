07dee68bf530de03af09f22d5b8d56ca128f1790
Ben <benjaminstevenburke@gmail.com>
2020-02-11 17:33:36 -0500

    master copy commit and push

--- movie-app-ui/src/App.tsx
+++ movie-app-ui/src/App.tsx
@@ -1,26 +1,79 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { FooterComponent } from './components/FooterComponent';
+import { render } from '@testing-library/react';
+import { ShowMovieComponent } from './components/ShowMovieComponent';
+import { LoginComponent } from './components/LoginComponent';
+import { RegisterComponent } from './components/RegisterComponent';
+import {  SearchMoviesComponent } from './components/SearchMoviesComponent';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+interface IAppState {
+  imdbId: string
+  movieSelected: boolean
+  userId: string
+}
+
+class App extends React.Component<any, IAppState> {
+  constructor(props:any){
+    super(props);
+    this.state={
+      imdbId: '',
+      movieSelected: false,
+      userId: '',
+
+    }
+  }
+  handleClick=(id:any)=>{
+    this.setState({
+      imdbId: id
+    })
+    
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <h1>MovieFriend</h1>
+       
+      <Router>
+        <Switch>
+        <Route
+              path='/'
+              exact
+              render={props => (
+                <SearchMoviesComponent {...props} handleClick={this.handleClick} />
+              )}
+            />
+
+
+            <Route
+            path={`/Movies/selected/${this.state.imdbId}`}
+            
+            render={props => (
+              <ShowMovieComponent
+                {...props}
+                imdbID={this.state.imdbId}
+                
+              />
+            )}
+          />
+          <Route
+          path='/login' component={LoginComponent}
+          />
+          <Route
+          path='/register' component={RegisterComponent}
+          />
+        </Switch>
+      </Router>
+      <FooterComponent/>
+      </div>
+    );
+  }  
 }
 
 export default App;
